4fe1e6d98eaa635534bf7e919504666209876302
Kevin Ho <kevinho122195@gmail.com>
2019-06-29 19:27:34 -0400

    Tests from learning java syntax, and also updated the README with some things I learned about JS

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # First-JS-Test
 Just my first javascript file following a tutorial
+
+Notes:
+-ctr+L to clear console
+-cmd+opt+i to bring up console in browser
+-all numbers are numbers, no float vs integer
+-dyanmic language: types of variables can be change, so a variable can be a string and you can change it to a number later
+-Arrays can have their lengths adjusted, you can also store diferent type of variables in the same array, an array can have both strings and numbers
--- index.js
+++ index.js
@@ -1,2 +1,30 @@
-// This is my first javascript code
-console.log('Hello World');
+/* let name ='Kevin';
+let age = 23;
+let person = {
+    name: name,
+    age:age
+}
+
+person.name = 'John';
+
+console.log(person.name); */
+
+//---------------------------------------------------------------------
+
+/* let selectedColors = ['red','blue'];
+selectedColors[2]='green';
+console.log(selectedColors.length); */
+
+//---------------------------------------------------------------------
+
+//performing a task
+/* function greet(name, lastName) {
+    console.log("Hello " + name + ' ' + lastName);
+}
+
+//calculates a value
+function square(number){
+    return number*number;
+} */
+
+
